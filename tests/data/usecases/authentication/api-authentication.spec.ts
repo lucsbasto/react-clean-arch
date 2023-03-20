9b697421f1649274fe32ef89d0ca5dbe88ad7028
Lucas Bastos <lucsbasto@gmail.com>
2023-03-20 09:23:57 -0300

    refactor: move HttpPostClientSpy to its own file

--- tests/data/usecases/authentication/api-authentication.spec.ts
+++ tests/data/usecases/authentication/api-authentication.spec.ts
@@ -1,23 +1,5 @@
-import { type IHttpPostClient } from '@/data/protocols/http/http-post-client'
-
-class HttpPostClientSpy implements IHttpPostClient {
-  url?: string
-  async post (url: string): Promise<void> {
-    this.url = url
-    return Promise.resolve()
-  }
-}
-
-class ApiAuthentication {
-  constructor (
-    private readonly url: string,
-    private readonly httpPostClient: IHttpPostClient
-  ) {}
-
-  async auth (): Promise<void> {
-    await this.httpPostClient.post(this.url)
-  }
-}
+import { HttpPostClientSpy } from '@/tests/mocks/mock-http-client'
+import { ApiAuthentication } from '@/src/data/usecases/authentication/api-authentication'
 
 describe('ApiAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
---
+++ tests/mocks/mock-http-client.ts
@@ -0,0 +1,9 @@
+import { type IHttpPostClient } from 'data/protocols/http/http-post-client'
+
+export class HttpPostClientSpy implements IHttpPostClient {
+  url?: string
+  async post (url: string): Promise<void> {
+    this.url = url
+    return Promise.resolve()
+  }
+}
