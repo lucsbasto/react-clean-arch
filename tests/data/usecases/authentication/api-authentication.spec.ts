276df2e3f4b67924c4c5718c15877eb3b4c0c8f2
Lucas Bastos <lucsbasto@gmail.com>
2023-03-20 11:25:53 -0300

    refactor: change ApiAuthentication to use fakerjs

--- tests/data/usecases/authentication/api-authentication.spec.ts
+++ tests/data/usecases/authentication/api-authentication.spec.ts
@@ -1,12 +1,12 @@
 import { HttpPostClientSpy } from '@/tests/mocks/mock-http-client'
 import { ApiAuthentication } from '@/src/data/usecases/authentication/api-authentication'
-
+import { faker } from '@faker-js/faker'
 type SutTypes = {
   sut: ApiAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new ApiAuthentication(url, httpPostClientSpy)
   return {
@@ -16,7 +16,7 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe('ApiAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
