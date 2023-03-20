64fd07cc22dd8ba3b75d5d5318df31f5afaf0f69
Lucas Bastos <lucsbasto@gmail.com>
2023-03-20 12:54:45 -0300

    feat: ensure ApiAuthentication returns an AccountModel if HttpPostClient returns stauts 200

--- src/data/usecases/authentication/api-authentication.ts
+++ src/data/usecases/authentication/api-authentication.ts
@@ -1,20 +1,20 @@
-import { AuthenticationParams } from '@src/domain/usecases/authentication'
+import { AuthenticationParams, IAuthentication } from '@src/domain/usecases/authentication'
 import { IHttpPostClient } from '@src/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@src/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@src/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@src/domain/errors/unexpected-error'
 import { AccountModel } from '@src/domain/models/account-model'
 
-export class ApiAuthentication {
+export class ApiAuthentication implements IAuthentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: IHttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const response = await this.httpPostClient.post({ url: this.url, body: params })
     switch (response.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return response.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
       default: throw new UnexpectedError()
     }
